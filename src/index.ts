eb6feaccfd5a9df6d7163e5e742039440dadb308
Tihomir Culig <turing.tculig@villagemd.com>
2024-03-13 19:27:36 +0100

    fix some errors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 const RaydiumSwapClass = require('./RaydiumSwap.ts')
 const solweb3 = require('@solana/web3.js')
-import { ApiPoolInfoV4, MARKET_STATE_LAYOUT_V3, Market, SPL_MINT_LAYOUT } from "@raydium-io/raydium-sdk";
-import base58 from "bs58";
+const rayray = require("@raydium-io/raydium-sdk");
+const b58 = require("bs58");
+const dotenv = require('dotenv');
+dotenv.config()
 
+const { ApiPoolInfoV4:_ApiPoolInfoV4, MARKET_STATE_LAYOUT_V3:_MARKET_STATE_LAYOUT_V3, Market:_Market, SPL_MINT_LAYOUT:_SPL_MINT_LAYOUT }= rayray;
 const sol = 'So11111111111111111111111111111111111111112' // e.g. SOLANA mint address
 const raydiumBuy = async (tokenAddress, amount, slippagePercentage) =>{
   return swap(sol,tokenAddress, amount, slippagePercentage);
@@ -70,25 +73,24 @@ const getConfirmation = async(signature) =>{
   return raydiumSwap.getConfirmation(signature);
 }
 
-const connectionSolanaHTTPS = new Connection("https://solana-mainnet.core.chainstack.com/"+process.env.RPC_URL, { commitment: 'confirmed' })
+const connectionSolanaHTTPS = new solweb3.Connection(process.env.RPC_URL, { commitment: 'confirmed' })
 async function callback(data: any) {
-  console.log(data)
   const programId = '675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8';
-  if (!data.filters.includes('transactionsSubKey')) return undefined
+  //if (!data.filters.includes('transactionsSubKey')) return undefined
 
-  const info = data.transaction
-  if (info.transaction.meta.err !== undefined) return undefined
+  const info = data;
+  if (info.meta.err !== undefined) return undefined
 
   const formatData: {
-    slot: number, txid: string, poolInfos: ApiPoolInfoV4[]
+    slot: number, txid: string, poolInfos: typeof _ApiPoolInfoV4[]
   } = {
     slot: info.slot,
-    txid: base58.encode(info.transaction.signature),
+    txid: b58.encode(info.transaction.signatures[0]),
     poolInfos: []
   }
 
-  const accounts = info.transaction.transaction.message.accountKeys.map((i: Buffer) => base58.encode(i))
-  for (const item of [...info.transaction.transaction.message.instructions, ...info.transaction.meta.innerInstructions.map((i: any) => i.instructions).flat()]) {
+  const accounts = info.transaction.message.staticAccountKeys.map((i: Buffer) => b58.encode(i))
+  for (const item of [...info.transaction.message.compiledInstructions, ...info.meta.innerInstructions.map((i: any) => i.instructions).flat()]) {
     if (accounts[item.programIdIndex] !== programId) continue
 
     if ([...(item.data as Buffer).values()][0] != 1) continue
@@ -103,9 +105,9 @@ async function callback(data: any) {
 
     if (baseMintAccount === null || quoteMintAccount === null || marketAccount === null) throw Error('get account info error')
 
-    const baseMintInfo = SPL_MINT_LAYOUT.decode(baseMintAccount.data)
-    const quoteMintInfo = SPL_MINT_LAYOUT.decode(quoteMintAccount.data)
-    const marketInfo = MARKET_STATE_LAYOUT_V3.decode(marketAccount.data)
+    const baseMintInfo = _SPL_MINT_LAYOUT.decode(baseMintAccount.data)
+    const quoteMintInfo = _SPL_MINT_LAYOUT.decode(quoteMintAccount.data)
+    const marketInfo = _MARKET_STATE_LAYOUT_V3.decode(marketAccount.data)
 
     formatData.poolInfos.push({
       id: accounts[keyIndex[4]],
@@ -127,7 +129,7 @@ async function callback(data: any) {
       marketVersion: 3,
       marketProgramId: marketAccount.owner.toString(),
       marketId: accounts[keyIndex[16]],
-      marketAuthority: Market.getAssociatedAuthority({ programId: marketAccount.owner, marketId: new solweb3.PublicKey(accounts[keyIndex[16]]) }).publicKey.toString(),
+      marketAuthority: _Market.getAssociatedAuthority({ programId: marketAccount.owner, marketId: new solweb3.PublicKey(accounts[keyIndex[16]]) }).publicKey.toString(),
       marketBaseVault: marketInfo.baseVault.toString(),
       marketQuoteVault: marketInfo.quoteVault.toString(),
       marketBids: marketInfo.bids.toString(),
